749aecf1c9226584bc4bfd71e9365910b32f2c87
Diptak Sengupta <diptak.iipmb@gmail.com>
2023-04-17 01:10:38 +0530

    2nd commit

---
+++ src/router/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import { Outlet , Link} from "react-router-dom";
+import { Fragment } from "react";
+import {ReactComponent as CrwnLogo} from '../../assets/crown.svg'
+import '../navigation/navigation.styles.scss'
+
+export const Navigation =()=>{
+    return(
+        <Fragment>
+            <div className="navigation">
+                <Link className='logo-container' to='/'>
+                       <CrwnLogo className="logo"/>
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to='/shop'>
+                        SHOP
+                    </Link>
+                    <Link className="nav-link" to='/sign-in'>
+                        SIGNIN
+                    </Link>
+                </div>
+            </div>
+            <Outlet/>
+        </Fragment>
+        
+    )
+}
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,57 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBwAPNO3hiSsc0P7jAaUbD3tGhipzfjIt0",
+    authDomain: "crwn-clothing-db-275e3.firebaseapp.com",
+    projectId: "crwn-clothing-db-275e3",
+    storageBucket: "crwn-clothing-db-275e3.appspot.com",
+    messagingSenderId: "830246351062",
+    appId: "1:830246351062:web:6a938738d9cc7306b35977"
+  };
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+}
+
