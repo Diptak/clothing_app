2d73b7cfa819eeb25036d08dfc9144ab92ddbfc6
Diptak Sengupta <diptak.iipmb@gmail.com>
2023-08-02 22:10:44 +0530

    added in our addToCart feature

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -13,12 +13,13 @@ const BUTTON_TYPE_CLASSES = {
     google: 'google-sign-in',
     inverted:'inverted'
 }
-export const Button = ({children, buttonType, ...otherProps}) =>{
+export const Button = ({children, buttonType, ...otherProps}) => {
     return (
-        <button className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+        <button 
+        className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
         {...otherProps}
         >
             {children}
         </button>
     )
-}
+};
